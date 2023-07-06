5c619ed4480461e8f5d80099a0eb77ee014f5175
anand103a1 <anand.srivastava103@gmail.com>
2023-07-06 12:44:13 +0530

    expenses js and css file created

--- src/App.js
+++ src/App.js
@@ -53,49 +53,49 @@ function App() {
 				title={expenses[0].title}
 				amount={expenses[0].amount}
 				date={expenses[0].date}
-			></ExpenseItem>
+			/>
 			<ExpenseItem
 				// expense={expenses[1]}
 				title={expenses[1].title}
 				amount={expenses[1].amount}
 				date={expenses[1].date}
-			></ExpenseItem>
+			/>
 			<ExpenseItem
 				// expense={expenses[2]}
 				title={expenses[2].title}
 				amount={expenses[2].amount}
 				date={expenses[2].date}
-			></ExpenseItem>
+			/>
 			<ExpenseItem
 				// expense={expenses[3]}
 				title={expenses[3].title}
 				amount={expenses[3].amount}
 				date={expenses[3].date}
-			></ExpenseItem>
+			/>
 			<ExpenseItem
 				// expense={expenses[4]}
 				title={expenses[3].title}
 				amount={expenses[3].amount}
 				date={expenses[3].date}
-			></ExpenseItem>
+			/>
 			<ExpenseItem
 				// expense={expenses[5]}
 				title={expenses[3].title}
 				amount={expenses[3].amount}
 				date={expenses[3].date}
-			></ExpenseItem>
+			/>
 			<ExpenseItem
 				// expense={expenses[6]}
 				title={expenses[3].title}
 				amount={expenses[3].amount}
 				date={expenses[3].date}
-			></ExpenseItem>
+			/>
 			<ExpenseItem
 				expense={expenses[7]}
 				title={expenses[3].title}
 				amount={expenses[3].amount}
 				date={expenses[3].date}
-			></ExpenseItem>
+			/>
 		</div>
 	);
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,15 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from './Card';
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-	const month = props.date.toLocaleString("en-US", { month: "long" });
-	const year = props.date.getFullYear();
-	const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-	// const expenseDate = new Date();
-	// const expenseTitle = "Car Purchase";
-	// const expenseAmount = '$247.54';
 	return (
 		<div className="expense-item">
+			<ExpenseDate date={props.date} />
 			{/* <div>{props.expense.date.toISOString()}</div> */}
 			{/* above prop is all about that we are adding the data at once to app  */}
 			{/* and below prop is all about that we are giving the data  one by one  */}
 			{/* <div>{props.date.toISOString()}</div> */}
-			<div>
-				<div>{month}</div>
-				<div>{year}</div>
-				<div>{day}</div>
-			</div>
 			{/* <div>{expenseDate.toISOString()}</div> */}
 			<div className="expense-item__description">
 				{/* <h2>{props.expense.title}</h2> */}
